bdea718ce84a8c4f2d6d5bcd399cd22a06d93916
Quinn Stephens <quinn@quinnstephens.com>
2012-07-13 19:48:36 -0500

    More stuffs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails3MongoidOmniauth::Application.routes.draw do
+  root :to => 'home#index'
+
   match '/auth/:provider/callback' => 'sessions#create'
   match '/auth/failure' => 'sessions#destroy'
   match '/signout' => 'sessions#destroy', :as => :signout
